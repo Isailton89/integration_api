6f99e9b3b194555b1323f55395c9e3a4332ed70b
Isailton Junio <113316233+Isailton89@users.noreply.github.com>
2024-05-07 13:43:03 -0300

    Delete src directory

--- src/main/java/com/isailton/appintegration/integration/IntegrationRepository.java
+++
@@ -1,6 +0,0 @@
-package com.isailton.appintegration.integration;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IntegrationRepository extends JpaRepository<Integration, String> {
-}
